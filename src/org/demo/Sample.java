0ca92cd11a5c31c26c57d3a8a30e30d374f6c78a
SivaNandhini1997 <nandynandhukannan@gmail.com>
2022-07-09 17:30:09 +0530

    Work Done

--- src/org/demo/Sample.java
+++ src/org/demo/Sample.java
@@ -5,10 +5,13 @@ public class Sample {
 	
 	public void m1() {
 		System.out.println("Siva.....");
+		System.out.println("Done Coding>>>>>>>>");
 	}
 	
 	public void m2() {
 		System.out.println("Code.......");
 	}
+	
+	
 	
 }
